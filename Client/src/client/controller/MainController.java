a6ecd826bd26af1b966c7e3c0bb607cbc2d45017
Neves <HelioNeves@users.noreply.github.com>
2017-06-11 23:56:21 -0300

    arquivos, interface e prevenção de erros

--- Client/src/client/controller/MainController.java
+++ Client/src/client/controller/MainController.java
@@ -5,13 +5,6 @@
  */
 package client.controller;
 
-import client.model.FileData;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -22,113 +15,193 @@ import javafx.scene.paint.Color;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
+import client.model.Conexao;
+import client.model.Files;
+import java.util.Optional;
+import javafx.scene.control.TextInputDialog;
+
 /**
  * FXML Controller class
  *
  * @author heliof
  */
 public class MainController {
-    @FXML Label serverIP;
-    @FXML Label serverStatus;
-    @FXML Label serverSends;
-    @FXML Label serverFaults;
-    @FXML ListView fileList;
-    @FXML Label tempoDecorrido;
-    @FXML Label tempoDecorridoPreT;
-    @FXML Label tempoDecorridoPosT;
-    @FXML ProgressBar progressBar;
-    
+
+    @FXML
+    Label serverIp;
+    @FXML
+    Label serverStatus;
+    @FXML
+    Label serverSends;
+    @FXML
+    Label serverFaults;
+    @FXML
+    ListView fileList;
+    @FXML
+    Label tempoDecorrido;
+    @FXML
+    Label tempoDecorridoPreT;
+    @FXML
+    Label tempoDecorridoPosT;
+    @FXML
+    ProgressBar progressBar;
+
+    Files file;
+    String fileName;
+
+    //Flag para testes sem o servidor
+    Boolean flagStandalone = false;
+
     //Flag para o temporizador
-    boolean flagTempo;
-    
-     // Use Java Collections to create the List.
-    List<String> list = new ArrayList<String>();
- 
-     // Now add observability by wrapping it with ObservableList.
-    ObservableList<String> observableList = FXCollections.observableList(list);  
-    
+    private boolean flagTempo;
+    private Conexao con;
+
     /**
      * Initializes the controller class.
      */
     @FXML
     public void initialize() {
-        // TODO
-        FileData myPath = new FileData();
-        
-        
-        observableList = myPath.proto();
-        
-        fileList.setItems(observableList);
-    }    
-    
+
+        file = new Files();
+
+        if (flagStandalone) {
+            con = new Conexao("localhost", 3000);
+
+            con.conectar();
+
+            if (con.statusDaConexao()) {
+                System.out.println("deu bom");
+            } else {
+                System.out.println("deu ruim");
+            }
+        }
+
+        do {
+            onFileDef();
+        } while (!populateIt());
+    }
+
     @FXML
-    public void onSend(){
-        bottomHandler(true);    
-        
+    public void onSend() {
+        bottomHandler(true);
+
         /*
-        Define a Task de contagem de tempo e atualização na tela
-        */
+        *   Pego o item selecionado da lista e coloco
+        *   no texto de qtd. de erros
+         */
+        serverFaults.setText(fileList.getSelectionModel().selectedItemProperty().getValue().toString());
+
+        /*
+        *Define a Task de contagem de tempo e atualização na tela
+         */
         flagTempo = true;
-        Task taskTempo = new Task<Void>(){
-            public Void call(){
+        Task taskTempo = new Task<Void>() {
+            public Void call() {
                 final long tempoInicial = System.currentTimeMillis();
-                while(flagTempo){
-                    try{
+                while (flagTempo) {
+                    try {
                         Thread.sleep(500);
-                    }catch(Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    Platform.runLater(new Runnable(){
+                    Platform.runLater(new Runnable() {
                         long tempoMillis;
                         float tempoSec;
-                        public void run(){
+
+                        public void run() {
                             tempoMillis = System.currentTimeMillis() - tempoInicial;
                             tempoSec = tempoMillis / 1000F;
                             tempoDecorrido.setText(Float.toString(tempoSec));
                         }
                     });
-                
+
                 }
                 return null;
             }
         };
-        
+
         new Thread(taskTempo).start(); //Inicia a thread com o contador de tempo
-        
-        serverIP.setText("192.168.1.1");
-        this.response(1);
-        
-        this.response(0);
+
+        this.response(true);
+        this.response(false);
+
         flagTempo = false; //Encerra o loop da thread de contagem de tempo
         taskTempo.cancel(); //Encerra a task de contagem de tempo
 
         bottomHandler(false);
     }
-    
+
     @FXML
-    public void onInfo(){
+    public void onInfo() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("UFCSPA - Programação Orientada a Objetos II");
         alert.setHeaderText("Aplicação cliente");
-        alert.setContentText("Autores:" + 
-                "\n" + "Gabriel Ramos dos Santos" +
-                "\n" + "Hélio Francisco das Neves Silveira Jr.");
+        alert.setContentText("Autores:"
+                + "\n" + "Gabriel Ramos dos Santos"
+                + "\n" + "Hélio Francisco das Neves Silveira Jr.");
 
-        alert.showAndWait();    
+        alert.showAndWait();
     }
-    
+
     @FXML
-    public void onReconnect(){
+    public void onReconnect() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Reconectando...");
         alert.setHeaderText(null);
-        alert.setContentText("Pronto!");
-        
+
+        //TODO
+        /*
+        * Implementar a reconexão aqui
+        * !
+        * !
+         */
+        //Caso sem conexão com o servidor, não checar status
+        if (flagStandalone) {
+            if (con.statusDaConexao()) {
+                alert.setContentText("Conectado!");
+            } else {
+                alert.setContentText("Ooops, verifique a conexão com o servidor e"
+                        + " sua conexão com a internet.");
+            }
+        } else {
+            alert.setContentText("Ooops, verifique a conexão com o servidor e"
+                    + " sua conexão com a internet.");
+        }
+
         alert.showAndWait();
     }
-    
-    public void bottomHandler(boolean key){
-        if(key){
+
+    @FXML
+    public void onIpDef() {
+        TextInputDialog dialog = new TextInputDialog("192.168.0.1");
+        dialog.setTitle("Conexão");
+        dialog.setHeaderText("Configuração do Conexão");
+        dialog.setContentText("IP:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        //TODO
+        /*
+        *   Implementar o getter e setter para o Ip
+        *   !
+         */
+        result.ifPresent(name -> System.out.println("Your name: " + name));
+    }
+
+    @FXML
+    public void onFileDef() {
+        TextInputDialog dialog = new TextInputDialog(file.getPath());
+        dialog.setTitle("Arquivos");
+        dialog.setHeaderText("Configuração do Caminho");
+        dialog.setContentText("Caminho:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(path -> file.setPath(path.replace('/', '\\')));
+    }
+
+    //Função para alternar o inferior da GUI
+    public void bottomHandler(boolean key) {
+        if (key) {
             tempoDecorrido.setTextFill(Color.BLACK);
             tempoDecorridoPreT.setTextFill(Color.BLACK);
             tempoDecorridoPosT.setTextFill(Color.BLACK);
@@ -139,13 +212,13 @@ public class MainController {
             progressBar.setProgress(0);
         }
     }
-    
+
     /*
     * função de resposta do servidor
-    * status, true para sucesso, false para falha
-    */
-    public void response(Integer status){
-        if(status > 0){
+    * status: true para sucesso, false para falha
+     */
+    public void response(Boolean status) {
+        if (status) {
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Sucesso!");
             alert.setHeaderText(null);
@@ -156,30 +229,33 @@ public class MainController {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Erro");
             alert.setHeaderText("Falha no envio!");
-            alert.setContentText("Ooops, confira a conexão com o servidor e" 
+            alert.setContentText("Ooops, verifique a conexão com o servidor e"
                     + " sua conexão com a internet.");
 
             alert.showAndWait();
         }
     }
-    
-    //Função genérica para popular a lista
-    public void populate(){
-        list.add("nudes.jpg");
-        list.add("bolo+de+leite+18.rar");
-        list.add("receita_dieta.zip");
-        list.add("receita_dieta2.zip");
-        list.add("receita_dieta3.zip");
-        list.add("receita_dieta4.zip");
-        list.add("receita_dieta5.zip");
-        list.add("receita_dieta6.zip");
-        list.add("organela.jpeg");
-        list.add("SsdasDIAJNONSDssd.bat");
+
+    //Função para popular a lista
+    public Boolean populateIt() {
+        try {
+            fileList.setItems(file.getFilesNames());
+            return true;
+        } catch (Exception e) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Cuidado!");
+            alert.setHeaderText("Arquivo Inválido");
+            alert.setContentText("¹Verifique o caminho de arquivo inserido"
+                    + " e tente novamente."
+                    + "\n" + "²Verifique se a pasta images existe.");
+
+            alert.showAndWait();
+            return false;
+        }
     }
 
+    //Função para adquirir o tempo decorrido
     public Label getTempoDecorrido() {
         return tempoDecorrido;
     }
-    
-    
 }
